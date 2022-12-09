a2109d1174bb86e7265da0bfa91e66bc16d02329
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2022-12-09 15:57:41 +0100

    Anhadir presentacion

---
+++ GestionTienda/codemr/GestionTienda 1/html/html_report_gestiontienda/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"GestionTienda working set: {GestionTienda}", "Number of Highly Problematic Classes":0, "Number of Entities":7, "Number of Problematic Classes":0, "Class Lines of Code":295, "Number of External Packages":4, "Number of Packages":2, "Number of External Entities":19}
---
+++ GestionTienda/codemr/GestionTienda 1/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,8 @@
+var matrix = [[0,1],[0,0]]
+var packages = [{
+"name": " es.unican.is2.gestionTienda.gui", "color": " #3182bd"
+}
+,{
+"name": " es.unican.is2.gestionTienda", "color": " #6baed6"
+}
+];
